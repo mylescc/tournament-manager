8e57bf874ab661598535c14490cc9bd2e9a9d349
Myles Cowper-Coles <myles@farmdrop.co.uk>
2015-04-21 22:26:31 +0100

    getting the test suite set up

---
+++ spec/factories/player_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :player do
+    name 'Myles'
+    bio  'A short bio about myself'
+  end
+end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe Player, type: :model do
+  let(:player) { create :player}
 
+  describe '#matches' do
+    it 'should have many matches' do
+      expect(player.matches).to be_empty
+    end
+  end
 end
