eb44ea749280481e5635d887a74cfd2e245e5393
Myles Cowper-Coles <myles@farmdrop.co.uk>
2015-04-26 15:56:20 +0100

    fixing tests

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 describe MatchesController, :type => :controller do
 
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET show" do
     let(:match) { create :match }
     it "returns http success" do
